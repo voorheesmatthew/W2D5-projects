38962244d50514b7040743d057b663605a31c5b0
Matt <voorhees.matthew@gmail.com>
2018-08-31 15:40:44 -0700

    complete inset and hasing(string)

--- MyHashMap/lib/p01_int_set.rb
+++ MyHashMap/lib/p01_int_set.rb
@@ -1,19 +1,26 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) {false}
   end
 
   def insert(num)
+    raise "Out of bounds" if num > @store.length || num < 0
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    return true if @store[num] == true
+    false
   end
 
   private
 
   def is_valid?(num)
+    
   end
 
   def validate!(num)
@@ -27,17 +34,26 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
+    # p @store
   end
 
   def remove(num)
+    self[num].delete(num)
+    # end
   end
 
   def include?(num)
+    self[num].each do |n|
+      return true if n == num
+    end
+    false
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -55,17 +71,40 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if @count == num_buckets
+      self[num] << num unless self.include?(num)
+      @count += 1
+      # num_buckets = num_buckets * 2 unless num_buckets.nil? 
+      @store = resize! 
+    end
+    
+    # @store = resize!
+    
+    unless self.include?(num)
+      self[num] << num 
+      @count += 1
+    end
+    
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].each do |n|
+      return true if n == num
+    end
+    false
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -74,5 +113,19 @@ class ResizingIntSet
   end
 
   def resize!
+    
+    new_store = Array.new(num_buckets * 2) {Array.new}
+    
+    @store.each do |bucket|
+      bucket.each do |element|
+        new_store[element % new_store.length] << element
+      end
+    end
+    
+    new_store
+    
+    # num_buckets = num_buckets * 2
+    # ResizingIntSet.new(num_buckets)
+    # @count = 0
   end
 end
--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    # mid = self.length / 2
+    # drop(mid) + take(mid)
+    # # self.to_s.to_i
+    # self.rotate(self.length - 1) 
+    # * self.reduce(:-)
+  self.join.to_i
   end
 end
 
 class String
   def hash
+    alpha = ("a".."z").to_a
+    convert = []
+    
+    self.each_char { |chr| convert << alpha.index(chr) }
+    convert.join.to_i
+    
   end
 end
 
